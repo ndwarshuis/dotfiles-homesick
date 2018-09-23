01a38c8f402b96650abf9ce6fa0549405ef54795
ndwarshuis <natedwarshuis@gmail.com>
2018-09-23 02:02:46 -0700

    this is annoying...add everything else

---
+++ home/.config/r/Rprofile.r
@@ -0,0 +1,10 @@
+.First <- function() {
+##  message("R is the best\n","working directory is:", getwd())
+}
+
+## Set CRAN mirror:
+local({
+  r <- getOption("repos")
+  r["CRAN"] <- "https://cloud.r-project.org/"
+  options(repos = r)
+})
